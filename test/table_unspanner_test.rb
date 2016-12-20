cbe5062665c6fe27e698aaf1917989df9ec5e80f
Tony Bowden <tony@tmtm.com>
2016-12-20 14:24:05 +0000

    Consolidate both test tables into one
    Make one example table, containing a th rowspan, a td rowspan, and a td
    colspan, and ensure that everything works smoothly, and then show the
    result with the same formatting to make it much more obvious what's
    going on.

--- test/table_unspanner_test.rb
+++ test/table_unspanner_test.rb
@@ -6,87 +6,96 @@ describe 'TableUnspanner' do
   end
 
   let(:unspanned_table) do
-    TableUnspanner::UnspannedTable.new(table_html)
+    TableUnspanner::UnspannedTable.new(given_table)
+  end
+
+  let(:noko) do
+    unspanned_table.nokogiri_node
   end
 
   describe 'UnspannedTable' do
     describe 'with rowspan attributes' do
-      let(:table_html) do
-        <<-TABLE
+      let(:given_table) do
+        <<-TABLE.gsub(/^ +/, '')
           <table>
             <tr>
               <th rowspan="3">Employees</th>
               <th>Name</th>
               <th>Role</th>
+              <th colspan="2">Start/End</th>
             </tr>
             <tr>
               <td>Alice</td>
-              <td rowspan="2">Test subject</td>
+              <td>Adviser</td>
+              <td rowspan="2">2016-01-01</td>
+              <td>2016-10-31</td>
             </tr>
             <tr>
               <td>Bob</td>
+              <td>Builder</td>
+              <td>2016-11-30</td>
             </tr>
           </table>
         TABLE
       end
 
-      it 'duplicates the <th> over three rows' do
-        unspanned_table.nokogiri_node.xpath('tr[1]/th[1]').text.must_equal 'Employees'
-        unspanned_table.nokogiri_node.xpath('tr[2]/th[1]').text.must_equal 'Employees'
-        unspanned_table.nokogiri_node.xpath('tr[3]/th[1]').text.must_equal 'Employees'
+      let(:expected_table) do
+        <<-TABLE.gsub(/^ +/, '')
+          <table>
+            <tr>
+              <th>Employees</th>
+              <th>Name</th>
+              <th>Role</th>
+              <th>Start/End</th>
+              <th>Start/End</th>
+            </tr>
+            <tr>
+              <th>Employees</th>
+              <td>Alice</td>
+              <td>Adviser</td>
+              <td>2016-01-01</td>
+              <td>2016-10-31</td>
+            </tr>
+            <tr>
+              <th>Employees</th>
+              <td>Bob</td>
+              <td>Builder</td>
+              <td>2016-01-01</td>
+              <td>2016-11-30</td>
+            </tr>
+          </table>
+        TABLE
       end
 
-      it 'duplicates the <td> over two rows' do
-        unspanned_table.nokogiri_node.xpath('tr[2]/td[2]').text.must_equal 'Test subject'
-        unspanned_table.nokogiri_node.xpath('tr[3]/td[2]').text.must_equal 'Test subject'
+      it 'gets the header row correct' do
+        row = noko.xpath('tr[1]/th').map(&:text)
+        row[0].must_equal 'Employees'
+        row[1].must_equal 'Name'
+        row[2].must_equal 'Role'
+        row[3].must_equal 'Start/End'
+        row[4].must_equal 'Start/End'
+      end
+
+      it 'gets Alice correct' do
+        noko.xpath('.//tr[2]/th').text.must_equal 'Employees'
+        row = noko.xpath('.//tr[2]/td').map(&:text)
+        row[0].must_equal 'Alice'
+        row[1].must_equal 'Adviser'
+        row[2].must_equal '2016-01-01'
+        row[3].must_equal '2016-10-31'
+      end
+
+      it 'gets Bob correct' do
+        noko.xpath('.//tr[3]/th').text.must_equal 'Employees'
+        row = noko.xpath('.//tr[3]/td').map(&:text)
+        row[0].must_equal 'Bob'
+        row[1].must_equal 'Builder'
+        row[2].must_equal '2016-01-01'
+        row[3].must_equal '2016-11-30'
       end
 
       it 'can be returned as a string' do
-        unspanned_table.html_string.must_equal <<-TABLE.strip
-<table>
-<tr>
-<th>Employees</th>
-<th>Name</th>
-<th>Role</th>
-</tr>
-<tr>
-<th>Employees</th>
-<td>Alice</td>
-<td>Test subject</td>
-</tr>
-<tr>
-<th>Employees</th>
-<td>Bob</td>
-<td>Test subject</td>
-</tr>
-</table>
-        TABLE
-      end
-    end
-
-    describe 'with colspan attributes' do
-      let(:table_html) do
-        <<-TABLE
-          <table>
-            <tr>
-              <th>Name</th>
-              <th>Role</th>
-              <th>Division</th>
-            </tr>
-            <tr>
-              <td>Alice</td>
-              <td colspan="2">Retired</td>
-            </tr>
-          </table>
-        TABLE
-      end
-
-      let(:cells) { unspanned_table.nokogiri_node.xpath('tr[2]/td') }
-
-      it 'replaces them with duplicate cells' do
-        cells.size.must_equal 3
-        cells[1].text.must_equal 'Retired'
-        cells[2].text.must_equal 'Retired'
+        unspanned_table.html_string.strip.must_equal expected_table.strip
       end
     end
   end
